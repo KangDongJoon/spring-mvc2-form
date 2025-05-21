874627a5e50c237002b4b2a6f21924b0bbcb3e16
KangDongJoon <ehdwns1119@naver.com>
2025-05-21 23:50:29 +0900

    라디오 버튼

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -4,6 +4,10 @@ public enum ItemType {
 
     BOOK("도서"), FOOD("음식"), ETC("기타");
 
+    public String getDescription() {
+        return description;
+    }
+
     private final String description;
 
     ItemType(String description) {
